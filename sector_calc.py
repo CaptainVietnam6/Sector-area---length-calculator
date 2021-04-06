0109c5c7c8e93cd21655620ade5d5de63bdc9caf
CaptainVietnam6 <kiet.riley2005@gmail.com>
2021-04-05 20:58:26 -1000

    Added sector area calculation

--- sector_calc.py
+++ sector_calc.py
@@ -2,6 +2,12 @@ calc_start = False
 start = input("Start? (y/n): ")
 DIGITS = int(input("Enter your accuracy (# digits of pi): "))
 
+def sector_parameter_input():
+    global radius
+    global angle
+    radius = float(input("Enter the circle's radius: "))
+    angle = float(input("Enter the angle of the arc: "))
+
 def pi_digits(x):
     k, a, b, a1, b1 = 2, 4, 1, 12, 4
     while x > 0:
@@ -17,16 +23,20 @@ def pi_digits(x):
 digits = [str(n) for n in list(pi_digits(DIGITS))]
 pi = float("%s.%s\n" % (digits.pop(0), "".join(digits)))
 
-def arc_calculator():
-    radius = float(input("Enter the circle's radius: "))
-    angle = float(input("Enter the angle of the arc: "))
+def sector_length_calc(radius, angle, pi):
     arc_length = 2 * pi * radius * angle / 360
-    print(f"\nArc length is {arc_length}\n")
+    print(f"\nSector arc length is {arc_length}")
+
+def sector_area_calc(radius, angle, pi):
+    arc_area = angle / 360 * pi * radius * radius
+    print(f"Sector area is {arc_area}\n")
 
 if start == "y" or start == "Y":
     calc_start = True
     while calc_start is True:
-        arc_calculator()
+        sector_parameter_input()
+        sector_length_calc(radius, angle, pi)
+        sector_area_calc(radius, angle, pi)
 
 elif start == "n" or start == "N":
     exit()
