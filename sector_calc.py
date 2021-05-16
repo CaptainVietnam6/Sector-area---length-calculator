8d929f0053f8200b9c19a1e0297ae24ad1dc9ab4
CaptainVietnam6 <kiet.riley2005@gmail.com>
2021-05-15 23:23:46 -1000

    minor changes including spacings

--- sector_calc.py
+++ sector_calc.py
@@ -9,12 +9,13 @@ Discord: CaptainVietnam6#7932
 Discord Server: https://discord.gg/3z76p8H5yj
 GitHub: https://github.com/CaptainVietnam6
 Instagram: @itz_kietttttttttt
-Program Status: ACTIVE, FINALISED
+Program Status: ACTIVE - FINALISED
 '''
 
 import time
 import os
 
+os.system("cls||clear")
 calc_start = False
 start = input("Start? (y/n): ")
 os.system("cls||clear")
@@ -22,8 +23,8 @@ os.system("cls||clear")
 if start == "y" or start == "Y":
     calc_start = True
     DIGITS = int(input("Enter your accuracy (# digits of pi): ")) #asks the user the precision or how many digits of pi the algorithm will generate
-    if DIGITS <=0 or DIGITS > 2500:
-        print("Sorry, but for caculation time reasons, this calculator will only accept under 2500 digits of pi")
+    if DIGITS <= 0 or DIGITS > 25000:
+        print("Sorry, but for caculation time reasons, this calculator will only accept under 25000 digits of pi")
         exit()
 
     #algorithm to generate requested digits of pi
@@ -36,7 +37,7 @@ if start == "y" or start == "Y":
             while d == d1 and x > 0:
                 yield int(d)
                 x -= 1
-                a, a1 = 10*(a % b), 10*(a1 % b1)
+                a, a1 = 10 * (a % b), 10 * (a1 % b1)
                 d, d1 = a/b, a1/b1
 
     digits = [str(n) for n in list(pi_digits(DIGITS))]
@@ -59,12 +60,14 @@ if start == "y" or start == "Y":
                 print("Sorry, but the input angle can only be between  1-360, please restart the calculator")
                 exit()
 
+
         #calculates the length of an arc from radius and angle
         def sector_length_calc(radius, angle, pi):
             global arc_length
             arc_length = 2 * pi * radius * angle / 360
             rounded_arc_length = round(arc_length, 2)
             print(f"\n\nSector arc length is {arc_length} (rounded to {rounded_arc_length})")
+
 
         #calculates area of the sector from radius and angle
         def sector_area_calc(radius, angle, pi):
@@ -73,12 +76,14 @@ if start == "y" or start == "Y":
             rounded_sector_area = round(sector_area, 2)
             print(f"Sector area is {sector_area} (rounded to {rounded_sector_area})\n")
 
+
         #calculates the circle's circumference from radius
         def circle_circumference_calc(radius, pi):
             global circle_circumference
             circle_circumference = 2 * pi * radius
             rounded_circle_circumference = round(circle_circumference, 2)
             print(f"The circle's circumference is {circle_circumference} (rounded to {rounded_circle_circumference})")
+
 
         #calculates the circle's area from radius
         def circle_area_calc(radius, pi):
@@ -87,17 +92,20 @@ if start == "y" or start == "Y":
             rounded_circle_area = round(circle_area, 2)
             print(f"The circle's area is {circle_area} (rounded to {rounded_circle_area})\n")
             
+
         #calculates what percentage is the arc's length in relation to the circle's circumference
         def length_percentage_calc(arc_length, circle_circumference):
             length_percentage = arc_length / circle_circumference * 100
             rounded_length_percentage = round(length_percentage, 2)
             print(f"The arc length is {length_percentage}% of the circle's circumference (rounded to {rounded_length_percentage}%)")
 
+
         #calculates what percentage is the sector's area in relation to the circle's area
         def sector_area_percentage_calc(sector_area, circle_area):
             sector_area_percentage = sector_area / circle_area * 100
             rounded_sector_area_percentage = round(sector_area_percentage, 2)
             print(f"The sector's area is {sector_area_percentage}% that of the circle's area (rounded to {rounded_sector_area_percentage}%)\n\n")
+
 
         sector_parameter_input()
         os.system("cls||clear") #clears the terminal
@@ -111,9 +119,10 @@ if start == "y" or start == "Y":
         length_percentage_calc(arc_length, circle_circumference)
         sector_area_percentage_calc(sector_area, circle_area)
 
+
 elif start == "n" or start == "N": #if the user responses with "n" it'll exit
     print("Exiting...")
-    time.sleep(float(0.5))  
+    time.sleep(float(0.5))
     exit()
 
 else: #if the user responds with anything that is not "y" or "n", it'll warn them and exit
